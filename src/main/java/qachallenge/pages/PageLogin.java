e18ff26e1a726c25702c128570732f766c5e0f7f
JairVega91 <55996383+ElchinoVega@users.noreply.github.com>
2021-06-06 23:32:44 -0500

    Add files via upload

---
+++ src/main/java/qachallenge/pages/PageLogin.java
@@ -0,0 +1,4 @@
+package qachallenge.pages;
+
+public class PageLogin {
+}
---
+++ src/test/java/qachallenge/base/BaseTest.java
@@ -0,0 +1,12 @@
+package qachallenge.base;
+
+import org.openqa.selenium.WebDriver;
+import
+
+public class BaseTest {
+
+    private static WebDriver driver;
+
+    public static
+
+}
